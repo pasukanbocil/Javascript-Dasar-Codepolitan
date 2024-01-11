4ea3a1175214dbf472c921191b614ad63e49f97b
pasukanbocil <ctian8787@gmail.com>
2024-01-11 13:11:50 +0700

    docs: :memo: Else IF

--- app.js
+++ app.js
@@ -9,14 +9,38 @@
 
 // console.log('Setelah Pembanding');
 
-let angka = Math.random();
-console.log(angka);
+// let angka = Math.random();
+// console.log(angka);
 
-if (angka>= 0.5) {
-    console.log('Angka Lebih Besar Dari 0.5');
-}
+// if (angka>= 0.5) {
+//     console.log('Angka Lebih Besar Dari 0.5');
+// }
 
-if (angka <= 0.5) {
-    console.log('Angka Lebih Kecil Dari 0.5');
+// if (angka <= 0.5) {
+//     console.log('Angka Lebih Kecil Dari 0.5');
     
-}
+// }
+
+// const iniHari ='Senin';
+
+// if (iniHari=== 'Senin') {
+//     console.log('Selamat Bekerja Kawan')
+// } else if(iniHari === 'Selasa'){
+//     console.log('Semoga Jangan Cepat Berlalu!')
+// }
+
+// const nilai = '40';
+
+// if (nilai < 50 ) {
+//     console.log('E');
+// } else if (nilai < 60) {
+//     console.log('D');
+    
+// }else if (nilai < 70) {
+//     console.log('C');
+    
+// }else if (nilai < 80) {
+//     console.log('B');
+// }else if (nilai < 100) {
+//     console.log('A');
+// }

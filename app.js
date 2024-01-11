be37cf90e6021efccebb2565ff0cb15f8f59d9fa
pasukanbocil <ctian8787@gmail.com>
2024-01-11 12:18:32 +0700

    feat: :memo: Materi IF

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-alert(1 + 1);
+// alert(1 + 1);
 
-console.log('Hello World!');
+// console.log('Hello World!');
+
+// console.log('Sebelum Pembanding');
+// if (1 + 1 === 2) {
+//     console.log('Didalam Pembanding');
+// }
+
+// console.log('Setelah Pembanding');
+
+let angka = Math.random();
+console.log(angka);
+
+if (angka>= 0.5) {
+    console.log('Angka Lebih Besar Dari 0.5');
+}
+
+if (angka <= 0.5) {
+    console.log('Angka Lebih Kecil Dari 0.5');
+    
+}
